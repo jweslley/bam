7efa84dab6352dbd9f81b6c434878a4bad53a641
Jonhnny Weslley <jw@jonhnnyweslley.net>
2013-12-01 21:24:40 -0300

    initial version

--- README.md
+++ README.md
@@ -1,4 +1,29 @@
-bam
-===
+# BAM!!!
 
-[work in progress]
+**Work in progress!!**
+
+
+## Configuration file syntax
+
+    [
+      {
+        "name": "monit",
+        "port": 2812
+      },
+      {
+        "name": "godoc",
+        "port": 3000
+      }
+    ]
+
+
+## TODO list
+
+* xip.io domains
+* SSL support
+* iptables rules
+* OSX support
+* Start scripts
+* Package installers
+* Process management
+* Docs
---
+++ bam.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"os"
+	"strings"
+)
+
+var (
+	configPath = flag.String("config", os.Getenv("HOME")+"/.bam/apps.json", "Config file")
+	httpAddr   = flag.String("http", ":9000", "HTTP service address")
+	tld        = flag.String("tld", getValue("LOCALTLD", "app"),
+		"Local top-level domain. Defaults to environment variable LOCALTLD")
+)
+
+func getValue(key, defaultValue string) string {
+	if value := os.Getenv(key); value == "" {
+		return defaultValue
+	} else {
+		return value
+	}
+}
+
+type App struct {
+	Name string `json:"name"`
+	Port int    `json:"port"`
+}
+
+func (a *App) Match(host, tld string) bool {
+	apphost := a.Name + "." + tld
+	return apphost == host || strings.HasSuffix(host, "."+apphost)
+}
+
+func (a *App) Host() string {
+	return fmt.Sprint("127.0.0.1:", a.Port)
+}
+
+type AppsProxy struct {
+	httputil.ReverseProxy
+	apps []App
+	tld  string
+}
+
+func (p *AppsProxy) GetAppHost(host string) (string, bool) {
+	for _, app := range p.apps {
+		if app.Match(host, p.tld) {
+			return app.Host(), true
+		}
+	}
+	return "", false
+}
+
+func NewAppsProxy(tld string) *AppsProxy {
+	p := &AppsProxy{tld: tld}
+	p.Director = func(req *http.Request) {
+		req.URL.Scheme = "http"
+		if host, found := p.GetAppHost(req.Host); found {
+			req.URL.Host = host
+		} else {
+			log.Println("[ERROR] bam: No host found: ", req.Host)
+		}
+	}
+	return p
+}
+
+var proxy = NewAppsProxy(*tld)
+
+func init() {
+	flag.Parse()
+
+	content, err := ioutil.ReadFile(*configPath)
+	if err != nil {
+		log.Fatalln("[ERROR]", err)
+	}
+
+	if err = json.Unmarshal(content, &proxy.apps); err != nil {
+		log.Fatalln("[ERROR]", err)
+	}
+}
+
+func main() {
+	log.Println("Starting HTTP server at", *httpAddr)
+	log.Fatal(http.ListenAndServe(*httpAddr, proxy))
+}
