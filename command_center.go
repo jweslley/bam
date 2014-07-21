15b35cbb6a92b5539a807b71b1bc88aa80a2693b
Jonhnny Weslley <jw@jonhnnyweslley.net>
2014-07-21 00:15:56 -0300

    add start/stop actions to ui

--- command_center.go
+++ command_center.go
@@ -32,14 +32,13 @@ func NewCommandCenter(c *Config) *CommandCenter {
 	return cc
 }
 
-func (cc *CommandCenter) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Content-Type", "text/html")
-	data := map[string]interface{}{
-		"Tld":            cc.tld,
-		"aliasedServers": cc.servers,
-		"apps":           cc.apps,
+func (cc *CommandCenter) List() []Server {
+	s := cc.servers
+	s = append(s, cc)
+	for _, app := range cc.apps {
+		s = append(s, app)
 	}
-	tmpl.Execute(w, data)
+	return s
 }
 
 func (cc *CommandCenter) Start() error {
@@ -50,16 +49,7 @@ func (cc *CommandCenter) Start() error {
 			cc.startApps()
 		}()
 	}
-	return http.ListenAndServe(fmt.Sprintf(":%d", cc.port), cc)
-}
-
-func (cc *CommandCenter) List() []Server {
-	s := cc.servers
-	s = append(s, cc)
-	for _, app := range cc.apps {
-		s = append(s, app)
-	}
-	return s
+	return http.ListenAndServe(fmt.Sprintf(":%d", cc.port), cc.createHandler())
 }
 
 func (cc *CommandCenter) startApps() {
@@ -67,6 +57,49 @@ func (cc *CommandCenter) startApps() {
 		log.Printf("Starting app %s\n", app.Name())
 		go app.Start()
 	}
+}
+
+func (cc *CommandCenter) createHandler() http.Handler {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", cc.index)
+	mux.HandleFunc("/start", cc.start)
+	mux.HandleFunc("/stop", cc.stop)
+	return mux
+}
+
+func (cc *CommandCenter) index(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/html")
+	data := map[string]interface{}{
+		"Tld":            cc.tld,
+		"aliasedServers": cc.servers,
+		"apps":           cc.apps,
+	}
+	tmpl.Execute(w, data)
+}
+
+func (cc *CommandCenter) start(w http.ResponseWriter, r *http.Request) {
+	cc.action(w, r, func(a *App) error {
+		log.Printf("Starting app %s\n", a.Name())
+		return a.Start()
+	})
+}
+
+func (cc *CommandCenter) stop(w http.ResponseWriter, r *http.Request) {
+	cc.action(w, r, func(a *App) error {
+		log.Printf("Stopping app %s\n", a.Name())
+		return a.Stop()
+	})
+}
+
+func (cc *CommandCenter) action(w http.ResponseWriter, r *http.Request, action func(a *App) error) {
+	name := r.URL.Query().Get("app")
+	for _, app := range cc.apps {
+		if app.Name() == name {
+			action(app)
+			break
+		}
+	}
+	http.Redirect(w, r, "/", 302)
 }
 
 func createAliasedServers(aliases map[string]int) []Server {
@@ -130,7 +163,16 @@ const baseHTML = `
 					{{ else }}
 						<li class="red">
 					{{ end }}
+						<div style="text-align: left">
 						<a href="http://{{.Name}}.{{$tld}}">{{.Name}}</a>
+						</div>
+						<div style="text-align: right">
+						{{ if .Started}}
+							<a href="http://bam.{{$tld}}/stop?app={{.Name}}">Stop</a>
+						{{ else }}
+							<a href="http://bam.{{$tld}}/start?app={{.Name}}">Start</a>
+						{{ end }}
+						</div>
 					</li>
 				{{end}}
       </ul>
